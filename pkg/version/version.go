7a48fbb538c0e891e060909c3248d381ab4a2f19
Steven Ceuppens <steven.ceuppens@icloud.com>
2019-04-04 10:58:47 +0200

    Added version logic

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -14,19 +14,87 @@
 
 package version
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // BuildVersion injected by the golang compiler
 var BuildVersion string
 
+// ErrVersionParse definition
+var ErrVersionParse = errors.New("Failed to parse BuildVersion string")
+
 // Version defines a version object that holds build information
 type Version struct {
+	Major         int
+	Minor         int
+	Patch         int
+	GitHash       string
+	GitAuthorDate string
+	GoVersion     string
+	GoArch        string
 }
 
 // FromString parses a version string to a version object
-func FromString(v string) Version {
-	return Version{}
+func FromString(v string) (*Version, error) {
+	fields := strings.Split(v, " ")
+	if len(fields) != 7 {
+		return nil, ErrVersionParse
+	}
+
+	vFields := strings.Split(fields[0], ".")
+	if len(vFields) != 3 {
+		return nil, ErrVersionParse
+	}
+
+	major, err := strconv.Atoi(vFields[0])
+	if err != nil {
+		return nil, ErrVersionParse
+	}
+
+	minor, err := strconv.Atoi(vFields[1])
+	if err != nil {
+		return nil, ErrVersionParse
+	}
+
+	patch, err := strconv.Atoi(vFields[2])
+	if err != nil {
+		return nil, ErrVersionParse
+	}
+
+	gitHash := fields[1]
+	if len(gitHash) != 7 {
+		return nil, ErrVersionParse
+	}
+
+	gitAuthorDate := fields[2]
+	if len(gitAuthorDate) != 25 {
+		return nil, ErrVersionParse
+	}
+
+	return &Version{
+		Major:         major,
+		Minor:         minor,
+		Patch:         patch,
+		GitHash:       gitHash,
+		GitAuthorDate: gitAuthorDate,
+		GoVersion:     fields[5],
+		GoArch:        fields[6],
+	}, nil
 }
 
 // ToString converts a version object to a string
-func ToString(v Version) string {
-	return ""
+func ToString(v *Version) string {
+	return fmt.Sprintf("%d.%d.%d %s %s go version %s %s",
+		v.Major, v.Minor, v.Patch,
+		v.GitHash, v.GitAuthorDate,
+		v.GoVersion, v.GoArch)
+}
+
+// Current returns the BuildVersion as Version object
+func Current() (*Version, error) {
+	return FromString(BuildVersion)
 }
--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -16,6 +16,7 @@ package version_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
 	. "github.com/opencicd/opencicd/pkg/version"
@@ -23,19 +24,127 @@ import (
 
 var _ = Describe("Version", func() {
 
-	Describe("Version", func() {
+	Describe("FromString", func() {
 
-		It("Should not be nil", func() {
-			Expect(&Version{}).NotTo(BeNil())
+	})
+
+	DescribeTable("FromString",
+		func(vString string, vObject *Version, errStatus error) {
+			v, err := FromString(vString)
+
+			if errStatus != nil {
+				Expect(err).To(Equal(errStatus))
+			}
+			Expect(v).To(Equal(vObject))
+		},
+		Entry(
+			"valid version string",
+			"1.2.3 95a4b3d 2019-04-04T10:08:26+02:00 go version go1.12.1 darwin/amd64",
+			&Version{
+				Major:         1,
+				Minor:         2,
+				Patch:         3,
+				GitHash:       "95a4b3d",
+				GitAuthorDate: "2019-04-04T10:08:26+02:00",
+				GoVersion:     "go1.12.1",
+				GoArch:        "darwin/amd64",
+			},
+			nil,
+		),
+		Entry(
+			"too many items",
+			"1.2.3 95a4b3d 2019-04-04T10:08:26+02:00 go version go1.12.1 darwin/amd64 foo bar",
+			nil,
+			ErrVersionParse,
+		),
+		Entry(
+			"too few items",
+			"foo bar",
+			nil,
+			ErrVersionParse,
+		),
+		Entry(
+			"invalid major version",
+			"a.2.3 95a4b3d 2019-04-04T10:08:26+02:00 go version go1.12.1 darwin/amd64",
+			nil,
+			ErrVersionParse,
+		),
+		Entry(
+			"invalid minor version",
+			"1.a.3 95a4b3d 2019-04-04T10:08:26+02:00 go version go1.12.1 darwin/amd64",
+			nil,
+			ErrVersionParse,
+		),
+		Entry(
+			"invalid patch version",
+			"1.2.a 95a4b3d 2019-04-04T10:08:26+02:00 go version go1.12.1 darwin/amd64",
+			nil,
+			ErrVersionParse,
+		),
+		Entry(
+			"too many version items",
+			"1.2.3.4 95a4b3d 2019-04-04T10:08:26+02:00 go version go1.12.1 darwin/amd64",
+			nil,
+			ErrVersionParse,
+		),
+		Entry(
+			"too few version items",
+			"1.2 95a4b3d 2019-04-04T10:08:26+02:00 go version go1.12.1 darwin/amd64",
+			nil,
+			ErrVersionParse,
+		),
+		Entry(
+			"invalid git hash",
+			"1.2.3 abc 2019-04-04T10:08:26+02:00 go version go1.12.1 darwin/amd64",
+			nil,
+			ErrVersionParse,
+		),
+		Entry(
+			"invalid git author date",
+			"1.2.3 95a4b3d 123abc go version go1.12.1 darwin/amd64",
+			nil,
+			ErrVersionParse,
+		),
+	)
+
+	Describe("ToString", func() {
+
+		It("Should create a valid string", func() {
+			vObject := &Version{
+				Major:         1,
+				Minor:         2,
+				Patch:         3,
+				GitHash:       "95a4b3d",
+				GitAuthorDate: "2019-04-04T10:08:26+02:00",
+				GoVersion:     "go1.12.1",
+				GoArch:        "darwin/amd64",
+			}
+
+			vString := ToString(vObject)
+
+			Expect(vString).To(Equal("1.2.3 95a4b3d 2019-04-04T10:08:26+02:00 go version go1.12.1 darwin/amd64"))
 		})
 	})
 
-	Describe("FromString", func() {
+	Describe("Current", func() {
 
-		It("Should return empty Version object if string is empty", func() {
-			v := FromString("")
+		It("Should create a valid version out of the BuildVersion", func() {
+			BuildVersion = "1.2.3 95a4b3d 2019-04-04T10:08:26+02:00 go version go1.12.1 darwin/amd64"
 
-			Expect(v).To(BeZero())
+			vObject := &Version{
+				Major:         1,
+				Minor:         2,
+				Patch:         3,
+				GitHash:       "95a4b3d",
+				GitAuthorDate: "2019-04-04T10:08:26+02:00",
+				GoVersion:     "go1.12.1",
+				GoArch:        "darwin/amd64",
+			}
+
+			v, err := Current()
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(v).To(Equal(vObject))
 		})
 	})
 })
